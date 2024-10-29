6568c8188774b00121f77c42931ceaa03059768e
songlongdaiduong <song.long.dai.duong@gmail.com>
2024-10-29 11:38:10 +0800

    Penguinlab: Add toggle for QTI BoostFramework [2/2]

--- src/com/penguin/settings/laboratory/LabSettings.java
+++ src/com/penguin/settings/laboratory/LabSettings.java
@@ -26,6 +26,7 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.SystemProperties;
+import androidx.preference.SwitchPreference;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import android.text.TextUtils;
@@ -55,6 +56,13 @@ implements
         getActivity().getContentResolver();
         addPreferencesFromResource(R.xml.penguin_lab_settings); final Resources res
         = getResources();
+        SwitchPreference qti_boost_framework = findPreference("qti_boost_framework");
+        qti_boost_framework.setChecked(SystemProperties.getBoolean("persist.qti.boost_framework.enabled", true));
+        qti_boost_framework.setOnPreferenceChangeListener((preference, newValue) -> {
+            boolean isEnabled = (Boolean) newValue;
+            SystemProperties.set("persist.qti.boost_framework.enabled", isEnabled ? "true" : "false");
+            return true;
+        });
     }
 
     @Override public boolean onPreferenceChange(Preference preference, Object
